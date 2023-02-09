4984b2c140380a902458db28a518379891919e2f
idreesnet <54152894+idreesnet@users.noreply.github.com>
2023-02-09 20:46:05 +0500

    completed, all 11 questions

--- chapters9-11/app.js
+++ chapters9-11/app.js
@@ -117,75 +117,161 @@ e.g "a" < "b", "n" > "m"
 
 /* Question No 6 
 ------------------*/
-var chemMarks = +prompt("Enter chemistry marks",);
-var pyhsicsMarks = +prompt("Enter physics marks",);
-var bioMarks = +prompt("Enter biology marks",);
-var totalObtainedMarks = chemMarks + pyhsicsMarks + bioMarks;
-var totalMarks = 300;
-var percentage = totalObtainedMarks / totalMarks * 100;
+// var chemMarks = +prompt("Enter chemistry marks",);
+// var pyhsicsMarks = +prompt("Enter physics marks",);
+// var bioMarks = +prompt("Enter biology marks",);
+// var totalObtainedMarks = chemMarks + pyhsicsMarks + bioMarks;
+// var totalMarks = 300;
+// var percentage = totalObtainedMarks / totalMarks * 100;
 
-var grade;
-var remarks;
+// var grade;
+// var remarks;
 
 
-/*------logic test for Fail--------*/
-if (percentage < 60) {
-    grade = "Fail";
-    remarks = "Sorry";
+// /*------logic test for Fail--------*/
+// if (percentage < 60) {
+//     grade = "Fail";
+//     remarks = "Sorry";
     
-    alert("Chemistry Marks: " + chemMarks +
-        "\nPhysics Marks: " + pyhsicsMarks +
-        "\nBiology Marks: " + bioMarks +
-        "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
-        "\nPercentage: " + percentage +
-        "% \nGrade: " + grade +
-        "\nRemarks: " + remarks);
-}
+//     alert("Chemistry Marks: " + chemMarks +
+//         "\nPhysics Marks: " + pyhsicsMarks +
+//         "\nBiology Marks: " + bioMarks +
+//         "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
+//         "\nPercentage: " + percentage +
+//         "% \nGrade: " + grade +
+//         "\nRemarks: " + remarks);
+// }
 
 
-/*------logic test for B Grade--------*/
-if (percentage >= 60 && percentage < 70) {
-    grade = "B";
-    remarks = "You need to improve";
+// /*------logic test for B Grade--------*/
+// if (percentage >= 60 && percentage < 70) {
+//     grade = "B";
+//     remarks = "You need to improve";
 
-    alert("Chemistry Marks: " + chemMarks +
-        "\nPhysics Marks: " + pyhsicsMarks +
-        "\nBiology Marks: " + bioMarks +
-        "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
-        "\nPercentage: " + percentage +
-        "% \nGrade: " + grade +
-        "\nRemarks: " + remarks);
+//     alert("Chemistry Marks: " + chemMarks +
+//         "\nPhysics Marks: " + pyhsicsMarks +
+//         "\nBiology Marks: " + bioMarks +
+//         "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
+//         "\nPercentage: " + percentage +
+//         "% \nGrade: " + grade +
+//         "\nRemarks: " + remarks);
     
-}
+// }
 
 
-/*------logic test for A Grade--------*/
-if (percentage >= 70 && percentage < 80) {
-    grade = "A";
-    remarks = "Good";
+// /*------logic test for A Grade--------*/
+// if (percentage >= 70 && percentage < 80) {
+//     grade = "A";
+//     remarks = "Good";
     
-    alert("Chemistry Marks: " + chemMarks +
-        "\nPhysics Marks: " + pyhsicsMarks +
-        "\nBiology Marks: " + bioMarks +
-        "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
-        "\nPercentage: " + percentage +
-        "% \nGrade: " + grade +
-        "\nRemarks: " + remarks);
+//     alert("Chemistry Marks: " + chemMarks +
+//         "\nPhysics Marks: " + pyhsicsMarks +
+//         "\nBiology Marks: " + bioMarks +
+//         "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
+//         "\nPercentage: " + percentage +
+//         "% \nGrade: " + grade +
+//         "\nRemarks: " + remarks);
 
+// }
+
+// /*------logic test for A-one Grade--------*/
+// if (percentage >= 80) {
+//     grade = "A-one";
+//     remarks = "Excellent";
+
+//     alert("Chemistry Marks: " + chemMarks +
+//         "\nPhysics Marks: " + pyhsicsMarks +
+//         "\nBiology Marks: " + bioMarks +
+//         "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
+//         "\nPercentage: " + percentage +
+//         "% \nGrade: " + grade +
+//         "\nRemarks: " + remarks);
+// }
+
+
+/*Question No 7
+--------------------*/
+// var secretNum = 8;
+// var guessNum = +prompt("Guess the secret number from 1 to 10,");
+//case a 
+//if(guessNum == secretNum){
+//     alert("Bingo! Correct answer");
+// }
+// case b
+//if(++guessNum ==secretNum){
+//     alert("Close enough to the correct answer");
+// }
+
+
+/*Questoin No 8
+-------------------*/
+// var numDiv3 = +prompt("Enter any number to check divisible by 3 or not");
+// if (numDiv3 % 3 == 0){
+//     alert("The number " + numDiv3 + " is divisible by 3");
+// }
+// else{
+//     alert("The number " + numDiv3 + " is not divisible by 3");
+// }
+
+
+/*Question No 9
+--------------------*/
+
+// var numCheck = +prompt("Enter a number to check EVEN or ODD");
+// if(numCheck%2 ==0){
+//     alert("The number "+ numCheck +" is EVEN");
+// }
+// else{
+//     alert("The number " + numCheck + " is ODD");
+// }
+
+
+/*Question No 10
+---------------------*/
+
+// var Temp = +prompt("Enter temperature");
+
+// if(Temp > 40){
+//     alert("It is too hot outside");
+// }
+
+// if(Temp > 30 && Temp <40 ){
+//     alert("The Weather today is Normal");
+// }
+
+// if(Temp > 20 && Temp < 30 ){
+//     alert("Today's Weather is cool");
+// }
+
+// if(Temp > 10 && Temp < 20 ){
+//     alert("OMG! Today's weather is so Cool");
+// }
+
+
+
+/* Question No 11
+--------------------*/
+
+var FirstNum = +prompt ("Enter first number");
+var SecondNum = + prompt("Enter second number");
+var operation = prompt("Enter the operation what you want");
+var compute ;
+if(operation === "+"){
+    alert(FirstNum + SecondNum);
 }
 
-/*------logic test for A-one Grade--------*/
-if (percentage >= 80) {
-    grade = "A-one";
-    remarks = "Excellent";
-
-    alert("Chemistry Marks: " + chemMarks +
-        "\nPhysics Marks: " + pyhsicsMarks +
-        "\nBiology Marks: " + bioMarks +
-        "\nTotal marks: 300 \nMarks obtained: " + totalObtainedMarks +
-        "\nPercentage: " + percentage +
-        "% \nGrade: " + grade +
-        "\nRemarks: " + remarks);
+if(operation == "-"){
+    alert(FirstNum - SecondNum);
 }
 
+if(operation == "*"){
+    alert(FirstNum * SecondNum);
+}
 
+if(operation == "/"){
+    alert(FirstNum / SecondNum);
+}
+
+if(operation == "%"){
+    alert(FirstNum % SecondNum);
+}
